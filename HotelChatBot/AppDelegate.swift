4788ac4979a29bdf22eab66d0e5aba833027bb8b
Jens Lünstedt <Jens.Luenstedt@ritz-schmidt.de>
2023-02-21 09:06:12 +0100

    Initial Commit

---
+++ HotelChatBot/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  HotelChatBot
+//
+//  Created by Jens Lünstedt on 21.02.23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ HotelChatBot/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HotelChatBot
+//
+//  Created by Jens Lünstedt on 21.02.23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
